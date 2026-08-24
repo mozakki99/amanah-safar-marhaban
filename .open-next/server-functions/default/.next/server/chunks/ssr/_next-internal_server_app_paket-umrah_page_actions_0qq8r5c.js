07dd1234c0ee93f9a79435a39104ec8eb9086fa1
Abdurrahman Muzakki <muzakki@MacBook-Zakki.local>
2026-08-24 19:35:26 +0700

    feat: add Cloudflare Worker marhaban config and gitignore

--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_paket-umrah_page_actions_0qq8r5c.js
+++
@@ -1,3 +0,0 @@
-module.exports=[74704,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_paket-umrah_page_actions_0qq8r5c.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qdxw3vL6AZifbud+SVybn6fFHV0phdzjHg30brE5Zs8=\"\n}"
